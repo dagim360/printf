5751576507a8d77ed8884053bded5c336ad927cf
dagim360 <dagim360@gmail.com>
2022-05-16 13:14:29 -0700

    commit it

--- _printf.c
+++ _printf.c
@@ -1,92 +1,28 @@
 #include "main.h"
+#include <stdarg.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * printIdentifiers - prints special characters
- * @next: character after the %
- * @arg: argument for the indentifier
- * Return: the number of characters printed
- * (excluding the null byte used to end output to strings)
- */
-
-int printIdentifiers(char next, va_list arg)
-{
-	int functsIndex;
-
-	identifierStruct functs[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"d", print_int},
-		{"i", print_int},
-		{"u", print_unsigned},
-		{"b", print_unsignedToBinary},
-		{"o", print_oct},
-		{"x", print_hex},
-		{"X", print_HEX},
-		{"S", print_STR},
-		{NULL, NULL}
-	};
-
-	for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
-	{
-		if (functs[functsIndex].indentifier[0] == next)
-			return (functs[functsIndex].printer(arg));
-	}
-	return (0);
-}
-
-/**
- * _printf - mimic printf from stdio
- * Description: produces output according to a format
- * write output to stdout, the standard output stream
- * @format: character string composed of zero or more directives
+ * _printf - printsand formats strings
  *
- * Return: the number of characters printed
- * (excluding the null byte used to end output to strings)
- * return -1 for incomplete identifier error
+ * @format: format string
+ *
+ * Return: number of charachters printed
  */
 
 int _printf(const char *format, ...)
 {
-	unsigned int i;
-	int identifierPrinted = 0, charPrinted = 0;
-	va_list arg;
+	int print_count = 0;
+	va_list arg_list;
 
-	va_start(arg, format);
-	if (format == NULL)
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			charPrinted++;
-			continue;
-		}
-		if (format[i + 1] == '%')
-		{
-			_putchar('%');
-			charPrinted++;
-			i++;
-			continue;
-		}
-		if (format[i + 1] == '\0')
-			return (-1);
+	va_start(arg_list, format);
 
-		identifierPrinted = printIdentifiers(format[i + 1], arg);
-		if (identifierPrinted == -1 || identifierPrinted != 0)
-			i++;
-		if (identifierPrinted > 0)
-			charPrinted += identifierPrinted;
+	print_count = hole(format, arg_list);
 
-		if (identifierPrinted == 0)
-		{
-			_putchar('%');
-			charPrinted++;
-		}
-	}
-	va_end(arg);
-	return (charPrinted);
+	va_end(arg_list);
+
+	return (print_count);
 }
--- main.h
+++ main.h
@@ -1,33 +1,61 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdarg.h>
-
-int _putchar(char c);
-int print_int(va_list arg);
-int print_unsigned(va_list arg);
-int _printf(const char *format, ...);
-int print_char(va_list arg);
-int print_str(va_list arg);
-int print_percent(void);
-void print_binary(unsigned int n, unsigned int* printed);
-int print_unsignedToBinary(va_list arg);
-int print_oct(va_list arg);
-int print_unsignedIntToHex(unsigned int num, char _case);
-int print_hex_base(va_list arg, char _case);
-int print_hex(va_list arg);
-int print_HEX(va_list arg);
-int print_STR (va_list arg);
+#include <limits.h>
 
 /**
- * struct identifierStruct - structure definition of a printTypeStruct
- * @indentifier: type
- * @printer: function to print
+ * struct map_t - a struct to map format specifier with functions
+ *
+ * @conversion_specifier: a char to represent a conversion specifier
+ * @function: address of corresponding function
  */
-typedef struct identifierStruct
+typedef struct map_t
 {
-char *indentifier;
-int (*printer)(va_list);
-} identifierStruct;
+	char conversion_specifier;
+	void (*function)(va_list arguments, int *count);
+} map;
 
-#endif
+/* main _printf function */
+int _printf(const char *format, ...);
+
+/*--------Utility functions---------*/
+void print_char(va_list args, int *count);
+void print_str(va_list args, int *count);
+void print_int(va_list args, int *count);
+void print_hex_x(va_list args, int *count);
+void print_hex_X(va_list args, int *count);
+
+/*--------Utility2 functions---------*/
+void print_bin(va_list args, int *count);
+void print_S(va_list args, int *count);
+void print_octal(va_list args, int *count);
+void print_rev(va_list args, int *count);
+char *convert(unsigned int num, int base);
+
+/*--------Utility3 functions---------*/
+void print_rot13(va_list args, int *count);
+void print_unsigned(va_list args, int *count);
+void print_address(va_list args, int *count);
+
+/*--------Helper functions---------*/
+/*helpers.c*/
+void reverse_string(char *string, int length);
+int _strlen(char *string);
+void _itoa(int num, char *string, int base);
+void print(char *, int *);
+void _putchar(char, int *);
+
+/*helpers_2.c*/
+int _numlen(int num, int base);
+void (*looper(char format))(va_list, int *);
+char transform(char x);
+void _itob(int num, char *string);
+void _itoux(unsigned int num, char *string, unsigned int base);
+
+/*helpers_3.c*/
+void _itop(unsigned long num, char *string, unsigned long base);
+
+#endif 
